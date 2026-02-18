8d0c9856c25ef4cc39da8f2356429f8713ee89d8
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-02-18 08:41:07 -0500

    Created order staging table
    This query creates or updates the main staging table for orders, casting various fields to appropriate data types.

---
+++ Adventure.01 — SQL Foundations & Data Modeling/sql/01_stg_order.sql
@@ -0,0 +1,29 @@
+--Query creates or updates main staging table
+
+CREATE OR REPLACE TABLE 
+query_quest.adventure_01.stg_orders
+AS
+SELECT 
+CAST("Row ID" AS INT)                AS row_id,
+CAST("Order ID" AS VARCHAR)             AS order_id, 	
+CAST("Order Date"	AS DATE)              AS order_date,
+CAST("Ship Date" AS	DATE)               AS ship_date,			
+CAST("Ship Mode" AS VARCHAR)            AS ship_mode,
+CAST("Customer ID" AS VARCHAR)          AS customer_id,
+CAST("Customer Name" AS VARCHAR)        AS customer_name,
+CAST("Segment" AS VARCHAR)              AS segment,
+CAST("Country" AS VARCHAR)              AS country,
+CAST("City" AS VARCHAR)                 AS city,
+CAST("State" AS VARCHAR)                AS state,
+LEFT(CAST("Postal Code" AS VARCHAR),5)  AS postal_code,
+CAST("Region" AS VARCHAR)               AS region,
+CAST("Product ID" AS VARCHAR)           AS product_id,
+CAST("Category" AS VARCHAR)             AS category,
+CAST("Sub-Category" AS VARCHAR)         AS sub_category,
+CAST("Product Name" AS VARCHAR)         AS product_name,
+CAST("Sales" AS DECIMAL(12,2))          AS sales,
+CAST("Quantity" AS INT)                 AS quantity,
+CAST("Discount" AS DECIMAL(5,4))        AS discount,
+CAST("Profit" AS DECIMAL(12,2))         AS profit
+FROM 
+query_quest.adventure_01.super_store

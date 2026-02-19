c48661d159aff380e89676a90c44290dff71fb6d
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-02-19 17:34:31 -0500

    Create fact_orders table for order data
    Create fact orders table with relevant columns for analysis.

---
+++ Adventure.01 — SQL Foundations & Data Modeling/sql/05_fact_orders.sql
@@ -0,0 +1,17 @@
+/*
+Create fact orders table with relevant secondary keys for joins
+*/
+CREATE OR REPLACE TABLE query_quest.adventure_01.fact_orders
+AS
+select 
+row_id,
+order_id,
+order_date,
+ship_date,
+ship_mode,customer_id,
+postal_code||city as city_code,
+product_id,
+sales,quantity,
+discount,
+profit
+from query_quest.adventure_01.stg_orders 

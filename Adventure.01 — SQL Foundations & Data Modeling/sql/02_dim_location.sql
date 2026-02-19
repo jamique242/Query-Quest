fc945bfc1cec2e0899f12e39f26ebeaacc8be093
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-02-18 21:57:57 -0500

    created dim_location table for shipping locations
    Create dim_location table from stg_orders for shipping locations.

---
+++ Adventure.01 — SQL Foundations & Data Modeling/sql/02_dim_location.sql
@@ -0,0 +1,14 @@
+/*
+Create or update dim location from staging table ... a pk was created as we found multiple cities
+to 1 postal code. dim location allows us to have an independent table for shipping location at a grain level
+*/
+
+CREATE OR REPLACE TABLE query_quest.adventure_01.dim_location 
+AS
+SELECT distinct
+city,
+state,
+postal_code,
+region,
+postal_code ||'-'||city as loc_key
+from query_quest.adventure_01.stg_orders
